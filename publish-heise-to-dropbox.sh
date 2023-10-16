bc8e7905a2e114cf12b2c442206c40c66e9be9e9
aews <alexander.wessel@web.de>
2023-10-16 23:08:05 +0200

    fix workflow

--- publish-heise-to-dropbox.sh
+++ publish-heise-to-dropbox.sh
@@ -1,7 +1,7 @@
 #!bin/sh
+mkdir -p ./.config/dbxcli/
 echo "{\"\":{\"personal\":\"$DROPBOX_PERSONAL_TOKEN\"}}" > ~/.config/dbxcli/auth.json
 dbxcli ls
 rm -f heise.epub
 ebook-convert heise.recipe heise.epub
-mkdir -p ./.config/dbxcli/
 dbxcli put ./heise.epub "/Apps/Rakuten Kobo/heise.epub"
